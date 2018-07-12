b4ee60c1d85256f18d9577d77131937d3b88ecf8
Jonathan Wrobel <jonathan.wrobel@omadahealth.com>
2018-07-12 13:05:37 -0700

    One for attempt by fixing the controller name and setting up other includes

--- app/controllers/current_player/active_streaks_controller.rb
+++ app/controllers/current_player/active_streaks_controller.rb
@@ -27,6 +27,7 @@ module CurrentPlayer
       logger.info "#{"*"*80}This is right before we have the inclusion of all the extra resources on the show"
       puts  "Here are the team_players: #{instance.team_players.map(&:color)}"
       render_jsonapi(instance, {
+        include: {:habits => {}, :players => {}, :team => {}, :team_players => {}},
         scope: false,
       })
     end
