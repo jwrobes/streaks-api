29c2c4695ebc0ba8371db37fbbc878764b9e0a2f
Jonathan Wrobel <jonathan.wrobel@omadahealth.com>
2018-08-25 22:56:02 -0700

    Fix redis to work in test and dev environments

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,8 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+rails_env = Rails.env || 'development'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+redis_uri = ENV["REDISTOGO_URL"] || "redis://#{redis_config[:host]}:#{redis_config[:port]}/12"
+uri = URI.parse(redis_uri)
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
