05906d001b68a86dfb3cd8dd523d2abebfb908f6
Jonathan Wrobel <jonathan.wrobel@omadahealth.com>
2018-08-25 22:58:18 -0700

    Adds comment to redis setup file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
+# Setup the redis for heroku and tests
 rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
 rails_env = Rails.env || 'development'
 redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
